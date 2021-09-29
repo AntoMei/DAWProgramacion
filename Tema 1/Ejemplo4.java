b50011e951c0fcf97a9dd9988702756fb185de19
AntoMei <antonio.meirosu444@gmail.com>
2021-09-29 10:35:52 +0200

    Hecho Ejemplo4

--- Tema 1/Ejemplo4.java
+++ Tema 1/Ejemplo4.java
@@ -1,3 +1,18 @@
-public class Ejemplo3 {
-	public static void main(String[] args) {
-	
+import java.util.Scanner;
+public class Ejemplo4 {
+    public static void main (String argv[]) {
+        float side;
+        float squareArea;
+
+        System.out.println("Enter the side:");
+
+        //Reading the value
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        side = inputValue.nextFloat();
+
+        squareArea = side * side;
+        System.out.println(squareArea);
+        inputValue.close();
+    }
+}
