09e3c382fc57517cbbc38ce1b5906789f61335f7
AntoMei <antonio.meirosu444@gmail.com>
2021-09-30 08:38:41 +0200

    Terminado Ej5

---
+++ Tema 1/Ej5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Ej5 {
+    public static void main (String argv[]) { 
+        int numero1;
+        int numero2;
+        int suma;
+        int resta;
+        int multiplicacion;
+        int division;
+
+        System.out.println("Escribe el primer numero:");
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        numero1 = inputValue.nextInt();
+
+        System.out.println("Escribe el segundo numero:");
+        Scanner inputValue2;
+        inputValue2 = new Scanner(System.in);
+        numero2 = inputValue2.nextInt();
+
+        suma = numero1 + numero2;
+        resta = numero1 - numero2;
+        multiplicacion = numero1 * numero2;
+        division = numero1 / numero2;
+
+        System.out.println("la suma del numero1 y numero2:"+suma);
+        System.out.println("la resta del numero1 y numero2:"+resta);
+        System.out.println("la multiplicacion del nuemero1 y numero2:"+multiplicacion);
+        System.out.println("la division del numero1 y numero2:"+division);
+    }
+}
