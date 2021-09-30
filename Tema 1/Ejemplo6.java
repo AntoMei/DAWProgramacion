853e805a2d307d8d4c4f828d0f43fad19eccfd11
AntoMei <90893283+AntoMei@users.noreply.github.com>
2021-09-30 12:50:54 +0200

    Hecho Ejemplo6

---
+++ Tema 1/Ejemplo6.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Ejemplo6 {
+    public static void main (String argv[]) {
+        float area;
+        final double PI = 3.14159;
+        int radio;
+        float length;
+
+        System.out.println("Pide el radio de una circunferencia");
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        radio = inputValue.nextInt();
+
+        length = (float) (2*PI*radio);
+        area = (float) (PI*radio*radio);
+
+        System.out.println("lengh:"+length);
+        System.out.println("area:"+area);
+    }
+}
+
