3232c870b53b1ecd6c3e9bd1cded55367d4d8288
AntoMei <antonio.meirosu444@gmail.com>
2021-10-06 10:23:06 +0200

    Modificado y finalizado Ejercicio 13

--- Tema 1/Ejemplo13.java
+++ Tema 1/Ejemplo13.java
@@ -1,4 +1,38 @@
 import java.util.Scanner;
 public class Ejemplo13 {
     public static void main (String argv[]) {
+        //Decalración de variables
+        double x,y;  
+        byte opcion;
+        Scanner inputValue = new Scanner(System.in);
+
+        //Empieza el programa
+        System.out.println("Introduce el valor 1");
+        x = inputValue.nextDouble();
+
+        System.out.println("Introduce el valor 2");
+        y = inputValue.nextDouble();
         
+        System.out.println("Ascendente (1) o Descendente (2)");
+        opcion = inputValue.nextByte();
+        
+        inputValue.close();
+        
+        if(opcion ==1) {
+            if(x<=y){
+            System.out.println(x+" "+y);
+        }
+        else{
+            System.out.println(y+" "+x);
+           }
+        }else 
+            if (opcion == 2){
+                if(x<=y){
+                    System.out.println(x+" "+y);
+                 }
+                 else{
+                     System.out.println(y+" "+x);
+        }
+    }
+    }
+}
