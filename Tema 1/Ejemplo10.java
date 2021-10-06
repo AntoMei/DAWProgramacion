18d87f9176cb9105e0d20352f500883132e6193e
AntoMei <antonio.meirosu444@gmail.com>
2021-10-06 09:34:18 +0200

    Modificado y finalizado Ejercicio 10

--- Tema 1/Ejemplo10.java
+++ Tema 1/Ejemplo10.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class Ejemplo10 {
     public static void main (String argv[]) {
         //Declaración de variables
-        short valor;
+        double valor;
         Scanner inputValue = new Scanner(System.in);
        
         //Empieza el proceso 
 
         System.out.println("Introduce el numero");
-        valor = inputValue.nextShort();
+        valor = inputValue.nextDouble();
         inputValue.close();
         if (valor >=0){
             System.out.println("Positivo");
--- Tema 1/Ejemplo12.java
+++ Tema 1/Ejemplo12.java
@@ -22,4 +22,3 @@ public class Ejemplo12 {
         inputValue.close();
     }
 }
-
