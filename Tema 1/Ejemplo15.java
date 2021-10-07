ae4099c8a8bcd97b9c3c650ee144b72abeb90d7f
AntoMei <antonio.meirosu444@gmail.com>
2021-10-07 11:57:01 +0200

    Finalizado Ejercicio 15

---
+++ Tema 1/Ejemplo15.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Ejemplo15 {
+    public static void main (String argv[]) {
+       //Declaración de variables
+        short numero;
+        Scanner inputValue = new Scanner(System.in);
+
+        //Empieza el programa
+        System.out.println("Introduce el numero y leelo");
+        numero = inputValue.nextShort();
+        inputValue.close();
+        if (numero ==1){
+            System.out.println("Uno");
+        }
+        if(numero==2){
+            System.out.println("Dos");
+        }
+        if(numero==3){
+            System.out.println("Tres");
+        }
+        if(numero==4){
+            System.out.println("Cuatro");
+        }
+        if(numero==5){
+            System.out.println("Cinco");
+        }
+        if(numero==6){
+            System.out.println("Seis");
+        }
+        if(numero==7){
+            System.out.println("Siete");
+        }
+        if(numero==8){
+            System.out.println("Ocho");
+        } 
+        if(numero==9){
+            System.out.println("Nueve");
+        }
+        if(numero==10){
+            System.out.println("Diez");
+        }
+    }
+}
