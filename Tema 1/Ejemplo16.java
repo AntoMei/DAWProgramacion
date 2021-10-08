020ce83e173cd29cb84af41c97d8377b4bc3ad0f
AntoMei <antonio.meirosu444@gmail.com>
2021-10-08 08:51:53 +0200

    Modificado y Finalizado Ej16

--- Tema 1/Ejemplo16.java
+++ Tema 1/Ejemplo16.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Ejemplo16 {
-    public static void main (String argv[]) {
+	public static void main(String args[]){
        //Declaración de variables
         int suma;
         int resta;
