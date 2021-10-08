ee7b5eabbffcafa41c61efa1075e13ad828e8cc2
AntoMei <antonio.meirosu444@gmail.com>
2021-10-08 08:49:05 +0200

    Modificado y Finalizado Ej9

--- Tema 1/Ejemplo9.java
+++ Tema 1/Ejemplo9.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Ejemplo9 {
-    public static void main (String argv[]) {
+	public static void main(String args[]){
       //Declaración de variables
       short edad = 0; 
       Scanner inputValue =  new Scanner(System.in);
