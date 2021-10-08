7644c2050124999bfedb67a69bde6b9e0a9a55a7
AntoMei <antonio.meirosu444@gmail.com>
2021-10-08 10:33:48 +0200

    Modificado y Finalizado Ej18

---
+++ Tema 1/Ejemplo18.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Ejemplo18 {
+    public static void main(String args[]){
+       //Declaración de variables
+       int horas, minutos, segundos;
+       Scanner inputValue = new Scanner(System.in);
+       
+       //Empieza el programa
+       System.out.println("Introduce la hora");
+       horas = inputValue.nextInt();
+       System.out.println("Introduce los minutos");
+       minutos = inputValue.nextInt();
+       System.out.println("Introduce los segundos");
+       segundos = inputValue.nextInt();
+       inputValue.close();
+
+       if (horas<24 && minutos<60 && segundos<60){
+           if (segundos<61 && segundos>=0){
+               System.out.println(horas+"horas"+minutos+"minutos"+(segundos+1)+"segundos");
+           }
+        }
+    }
+}
