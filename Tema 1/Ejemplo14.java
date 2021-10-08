00f85dcea5bf82475bff9ed9e88d2136a3ac95e2
AntoMei <antonio.meirosu444@gmail.com>
2021-10-08 09:50:21 +0200

    Modificado y Finalizado Ej14

--- Tema 1/Ejemplo14.java
+++ Tema 1/Ejemplo14.java
@@ -10,11 +10,19 @@ public class Ejemplo14 {
       System.out.println("Introduce la nota");
       nota = inputValue.nextByte();
       inputValue.close();
-      if (nota<=0){
-      }else{
-          if(nota<3){
-        System.out.println("Muy Deficiente");
-          }
-        }
+
+      if ((nota>=0)&&(nota<3)){
+          System.out.println("Muy Deficiente");
+      }else if((nota>=3)&&(nota<5)){
+          System.out.println("Insuficiente");
+      }else if((nota>=5)&&(nota<6)){
+          System.out.println("Suficiente");
+      }else if((nota>=6)&&(nota<7)){
+          System.out.println("Bien");
+      }else if((nota>=7)&&(nota<9)){
+          System.out.println("Notable");
+      }else if((nota>=9)&&(nota<=10)){
+          System.out.println("Sobresaliente");
       }
     }
+  }
