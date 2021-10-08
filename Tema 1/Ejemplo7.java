a5123192cd166e3f478ee753338ad6cece4368fb
AntoMei <antonio.meirosu444@gmail.com>
2021-10-08 08:48:18 +0200

    Modificado y Finalizado Ej7

--- Tema 1/Ejemplo7.java
+++ Tema 1/Ejemplo7.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Ejemplo7 {
-    public static void main (String argv[]) {
+	public static void main(String args[]){
         //Declaración de variables
         double precio;
         double precioRebajado;
