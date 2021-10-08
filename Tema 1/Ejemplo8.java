1445c39f6a2ead6a09db97f1bcaf6d941ba143fb
AntoMei <antonio.meirosu444@gmail.com>
2021-10-08 08:48:40 +0200

    Modificado y Finalizado Ej8

--- Tema 1/Ejemplo8.java
+++ Tema 1/Ejemplo8.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Ejemplo8 {
-    public static void main (String argv[]) {
+    public static void main(String args[]){
         
         //Declaración de variables
         short edad = 0; 
