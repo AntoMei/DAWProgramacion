d9d53d69d38c7948d95d180dc6534ec0991d317b
AntoMei <antonio.meirosu444@gmail.com>
2021-10-13 13:35:33 +0200

    Modificao y sin terminar Ej24

--- Tema 1/Ejemplo24.java
+++ Tema 1/Ejemplo24.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class Ejemplo24 {
-    
+    public static void main(String args[]){
+    Scanner inputValue = new Scanner(System.in);
+    int numero =0;
+    double total = 0;
+    int contador = 0;
+
+    System.out.println("Lee el conjunto de notas");
+        
+        do {
+            numero = inputValue.nextInt();
+            total = total + numero;
+            contador++;
+        inputValue.close();
+        
+        
+    }while (numero!=1);
+        System.out.println("Media:"+total/contador);
+}
 }
