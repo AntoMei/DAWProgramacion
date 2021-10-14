80db552f1064befe0496af2deba81508af2dec24
AntoMei <antonio.meirosu444@gmail.com>
2021-10-14 11:44:44 +0200

    Modificado Ej23

--- Tema 1/Ejemplo19.java
+++ Tema 1/Ejemplo19.java
@@ -3,16 +3,15 @@ public class Ejemplo19 {
     public static void main(String args[]){
        //Declaración de variables
        int horas;
-       double precioporhora;
-       double pagobruto, salarioneto, impuestos;
+       double impuestos;
        Scanner inputValue = new Scanner(System.in);
        
        //Empieza el programa
-       System.out.println("Introduce el precio/hora");
-       precioporhora = inputValue.nextDouble();
        System.out.println("Introduce el número de horas");
        horas = inputValue.nextInt();
        inputValue.close();
        
+       //Más de 35 horas
        if(horas>35){
-           System.out.println()
+           System.out.println(35*1.5);
+    
--- Tema 1/Ejemplo23.java
+++ Tema 1/Ejemplo23.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 public class Ejemplo23 {
     public static void main(String args[]){
         //Declaración de variables
-        int numero;
+        int numero= 0;
+        int positivos= 0;
         Scanner inputValue = new Scanner (System.in);
 
-        
+        //Empieza el programa
+        do
+        {
+        System.out.println("Introduce los números:");
+        numero = inputValue.nextInt();
+        positivos=positivos+numero;
+        }while(numero!=0);
+            System.out.println("Para terminar el programa pulse 0:");
+            inputValue.close();
+    }
+}
