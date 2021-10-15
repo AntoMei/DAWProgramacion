7bc03bf80d2c3b53e47c76283a9fe4ae1e2fecc5
AntoMei <antonio.meirosu444@gmail.com>
2021-10-15 09:17:40 +0200

    Modificado y Finalizado Ej22

--- Tema 1/Ejemplo22.java
+++ Tema 1/Ejemplo22.java
@@ -2,20 +2,21 @@ import java.util.Scanner;
 public class Ejemplo22 {
     public static void main(String args[]){
         //Declaración de variables
-        int numero; contador; positivos;
+        int numero; 
+        int contador = 0;
+        int positivos = 0;
         Scanner inputValue = new Scanner (System.in);
 
          //Empieza el programa
          System.out.println("Escribe 10 números");
-        for (numero = 0; numero < 10; numero++){
+        while(contador<10){
             numero = inputValue.nextInt();
-         inputValue.close();
-         if (numero>=0) {
-             System.out.println("Positivos");
-             
- 
-
-         }
+            if (numero>=0){
+                positivos = positivos + 1;
+            }
+            contador = contador +1;
         }
+        inputValue.close();
+        System.out.println("Hay"+positivos+"números positivos");
     }
 }
