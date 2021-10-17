2731fe540ffc1209f93cc5473c90f4f052faae01
AntoMei <antonio.meirosu444@gmail.com>
2021-10-17 19:09:43 +0200

    Modificado Ej31

--- Tema 1/Ejemplo31.java
+++ Tema 1/Ejemplo31.java
@@ -14,7 +14,7 @@ public class Ejemplo31 {
         anArray[8]=900;
         anArray[9]=1000;
 
-        for( int i = 0; i< anArray.length; i++){
+        for( int anA = 10; 10< anArray.length; 10++){
             System.out.println(anArray[10]);
         }
     }
