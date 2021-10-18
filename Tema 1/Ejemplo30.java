aea573de0a319cbbf7e4ec00501177095e2da15f
AntoMei <antonio.meirosu444@gmail.com>
2021-10-18 09:47:01 +0200

    Modificado Ej30

--- Tema 1/Ejemplo30.java
+++ Tema 1/Ejemplo30.java
@@ -1,22 +1,17 @@
+import java.util.Scanner;
 public class Ejemplo30 {
     public static void main(String[] args) {
-        String palabra =
-        int inicio = 0;
-        int termina = palabra.length()-1;
-        boolean bError = false;
+        Scanner inputValue = new Scanner(System.in);
+        String palabra = inputValue.nextLine();
 
-        while ((inicio<termina)&& (bError)){
-            if(palabra.charAt(inicio)==palabra.charAt(termina)){
-                inicio++;
-                termina--;
-            }else{
-                bError = true;
+        int contador = 1;
+        for (int i = 0; i < palabra.length(); i++){
+
+            if (palabra.charAt(i) != palabra.charAt(palabra.length()-1-1)){
+                contador = 0; break;
             }
-        }
-        if (!bError){
-            System.out.println(palabra+"es un palindromo");
-        }else{
-            System.out.println(palabra+"No es un palindromo");
+            inputValue.close();
+            System.out.println(contador ==1 ?"es palindromo":"no es palindromo");
         }
     }
 }
