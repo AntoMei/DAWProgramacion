6e546d82fd156b8aabf3154795aa04e625d9c9a0
AntoMei <antonio.meirosu444@gmail.com>
2021-10-18 08:55:56 +0200

    Modificado y Finalizado Ej19

--- Tema 1/Ejemplo19.java
+++ Tema 1/Ejemplo19.java
@@ -3,15 +3,37 @@ public class Ejemplo19 {
     public static void main(String args[]){
        //Declaración de variables
        int horas;
-       double impuestos;
+       float precio;
+       float bruto;
+       float impuestos;
        Scanner inputValue = new Scanner(System.in);
        
        //Empieza el programa
-       System.out.println("Introduce el número de horas");
+       System.out.println("Introduce las horas");
        horas = inputValue.nextInt();
+       System.out.println("Introduce las horas");
+       System.out.println("Introduce el precio por hora");
+       precio = inputValue.nextFloat();
+
+       //Horas
+       if(horas<=35){
+          bruto = horas * precio;
+       }else{
+        bruto = 35 * precio;
+        bruto += ((horas -35) * precio * 1.5);
+       }
+       //Impuestos
+       if(bruto<=500){
+           impuestos=0;
+       }else if (bruto<=900){
+           impuestos=(bruto-500) * 0.25f;
+       }else{
+           impuestos = (400-0.25f) + (bruto-500-400) * 0.45f;
+       }
        inputValue.close();
-       
-       //Más de 35 horas
-       if(horas>35){
-           System.out.println(35*1.5);
-    
+
+       System.out.println("Pago bruto"+bruto);
+       System.out.println("Salario neto"+(bruto - impuestos));
+       System.out.println("Impuestos"+impuestos);
+    }
+}
