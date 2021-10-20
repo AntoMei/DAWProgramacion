b3f65fe17558e1dca8699948e8bfbfb0aff3fbdd
AntoMei <antonio.meirosu444@gmail.com>
2021-10-20 10:17:17 +0200

    Modificado y Finalizado Ej33

--- Tema 1/Ejemplo33.java
+++ Tema 1/Ejemplo33.java
@@ -3,21 +3,13 @@ public class Ejemplo33 {
     public static void main(String[] args) {
         Scanner inputValue = new Scanner(System.in);
         int numeroDNI;
-        System.out.println("Introduce tu número de DNI:");
+        String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+        System.out.println("Introduce tu número de DNI sin letra:");
         numeroDNI = inputValue.nextInt();
-        System.out.printf("La letra que corresponde a %d es %c", numeroDNI, calcularLetra(nuemroDNI));
         inputValue.close();
-    }
 
-    private static char calcularLetra(int numeroDNI){
-        String miLetra="TRWAGMYFPDXBNJZSQVHLCE";
-        int resto = numeroDNI % 23;
-        return miLetra.charAt(resto);
-    }
-
-    private static char calcularLetraArray(int numeroDNI){
-        char caracteres[] = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
-        int resto = numeroDNI % 23;
-        return caracteres[resto];
+        letras.charAt(numeroDNI % 23);
+        System.out.println("El numero de DNI es:"+numeroDNI+"-" +letras.charAt(numeroDNI%23));
     }
 }
