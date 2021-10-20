cfbd32b388e26436dd42f4a068b4171e754ab4a7
AntoMei <antonio.meirosu444@gmail.com>
2021-10-20 08:30:28 +0200

    Cambios Ej30

--- Tema 1/Ejemplo30.java
+++ Tema 1/Ejemplo30.java
@@ -1,17 +1,28 @@
 import java.util.Scanner;
 public class Ejemplo30 {
     public static void main(String[] args) {
-        Scanner inputValue = new Scanner(System.in);
-        String palabra = inputValue.nextLine();
+       String sPalabra = "dabale arroz a la zorra el abad";
+       Scanner inputValue = new Scanner (System.in);
+       int inc =  0;
+       int des = sPalabra.length()-1;
+       boolean bError = false;
 
-        int contador = 1;
-        for (int i = 0; i < palabra.length(); i++){
+       System.out.println("Introduzca una palabra o una frase:");
+       sPalabra = new inputValue.nextLine());
 
-            if (palabra.charAt(i) != palabra.charAt(palabra.length()-1-1)){
-                contador = 0; break;
-            }
-            inputValue.close();
-            System.out.println(contador ==1 ?"es palindromo":"no es palindromo");
+       while ((inc<des)&&(!bError)){
+           if (sPalabra.charAt(inc)==sPalabra.charAt(des)){
+               inc ++;
+               des --;
+           }else{
+               bError = true;
+           }
         }
+        if (!bError)
+            System.out.println(sPalabra + "es un palindromo");
+            
+        else
+
+            System.out.println(sPalabra + "no es un palindromo");
     }
 }
