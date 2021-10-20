f523487be2fb1aeeff23348c728b26dbd32fe211
AntoMei <antonio.meirosu444@gmail.com>
2021-10-20 09:45:34 +0200

    Cmabios Ej30 y 32

--- Tema 1/Ejemplo30.java
+++ Tema 1/Ejemplo30.java
@@ -1,28 +1,5 @@
 import java.util.Scanner;
 public class Ejemplo30 {
     public static void main(String[] args) {
-       String sPalabra = "dabale arroz a la zorra el abad";
-       Scanner inputValue = new Scanner (System.in);
-       int inc =  0;
-       int des = sPalabra.length()-1;
-       boolean bError = false;
-
-       System.out.println("Introduzca una palabra o una frase:");
-       sPalabra = new inputValue.nextLine());
-
-       while ((inc<des)&&(!bError)){
-           if (sPalabra.charAt(inc)==sPalabra.charAt(des)){
-               inc ++;
-               des --;
-           }else{
-               bError = true;
-           }
-        }
-        if (!bError)
-            System.out.println(sPalabra + "es un palindromo");
-            
-        else
-
-            System.out.println(sPalabra + "no es un palindromo");
-    }
-}
+        Scanner inputValue = new Scanner(System.in);
+        
--- Tema 1/Ejemplo33.java
+++ Tema 1/Ejemplo33.java
@@ -3,20 +3,21 @@ public class Ejemplo33 {
     public static void main(String[] args) {
         Scanner inputValue = new Scanner(System.in);
         int numeroDNI;
-        int resto = 0;
-        String miLetra="";
-        String[]letra = {"T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
-        
-       return miLetra[numeroDNI _% 23];
-
         System.out.println("Introduce tu número de DNI:");
         numeroDNI = inputValue.nextInt();
+        System.out.printf("La letra que corresponde a %d es %c", numeroDNI, calcularLetra(nuemroDNI));
         inputValue.close();
-       
-        do{
-            System.out.println("Introduce tu número de DNI:");
-            numeroDNI = inputValue.nextInt();
-        }while (numeroDNI<=0);
-        System.out.println(+numeroDNI+"-"+miLetra);
     }
-}     
+
+    private static char calcularLetra(int numeroDNI){
+        String miLetra="TRWAGMYFPDXBNJZSQVHLCE";
+        int resto = numeroDNI % 23;
+        return miLetra.charAt(resto);
+    }
+
+    private static char calcularLetraArray(int numeroDNI){
+        char caracteres[] = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+        int resto = numeroDNI % 23;
+        return caracteres[resto];
+    }
+}
