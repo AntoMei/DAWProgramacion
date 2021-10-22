8c71781030cfb86c222266cbfca03f847924c2b1
AntoMei <antonio.meirosu444@gmail.com>
2021-10-22 08:40:35 +0200

    Cambios Ej34, 35, 36

--- Tema 1/Ejemplo30.java
+++ Tema 1/Ejemplo30.java
@@ -2,4 +2,9 @@ import java.util.Scanner;
 public class Ejemplo30 {
     public static void main(String[] args) {
         Scanner inputValue = new Scanner(System.in);
+        boolean palindromo = true;
+        int palabra;
+        int mitadPalabra;
         
+
+        System.out.println("Introduce una palabra o frase:")
