a517ad9322798f4a95514adca5ed97018afbb0d6
AntoMei <antonio.meirosu444@gmail.com>
2021-10-25 09:49:09 +0200

    Cambios

--- Tema 1/Ejemplo35.java
+++ Tema 1/Ejemplo35.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
 public class Ejemplo35 {
-    
+    public static void main(String args[]){
+    int numero[] = new int[10];
+    double media = 0;
+    int nota;
+    Scanner lector = 
+
+    System.out.println("Introduce las notas:");
+    nota = lector.nextInt();
+
+    for (int i= 0; i< numero.length; i++){
+        media = media + numero[i];
+    }
+    media = media / numero.length;
+}
 }
--- Tema 1/Ejemplo36.java
+++ Tema 1/Ejemplo36.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 public class Ejemplo36 {
     public static void main(String[] args) {
         int numeros[] = new int [10];
-        int maximo = numeros[0];
+        int n;
+        int mayor;
+        Scanner lector = new Scanner (System.in);
+      
         for (int i = 0; i < 10; i++){
-            if (maximo < numeros[i]){
-                maximo = numeros[i];
+            System.out.println("Ingresa un número:");
+            n = lector.nextInt();
+            if(numeros>mayor){
+                mayor = numeros;
             }
         System.out.println("El maximo de los números es:"+maximo);
         }
