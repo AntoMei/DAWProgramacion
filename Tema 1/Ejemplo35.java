7f38679f351a19b796fad4c05793a46eeecffb15
AntoMei <antonio.meirosu444@gmail.com>
2021-10-27 08:12:11 +0200

    Cambios

--- Tema 1/Ejemplo35.java
+++ Tema 1/Ejemplo35.java
@@ -1,14 +1,8 @@
-import java.util.Scanner;
 public class Ejemplo35 {
     public static void main(String args[]){
     int numero[] = new int[10];
     double media = 0;
-    int nota;
-    Scanner lector = 
-
     System.out.println("Introduce las notas:");
-    nota = lector.nextInt();
-
     for (int i= 0; i< numero.length; i++){
         media = media + numero[i];
     }
