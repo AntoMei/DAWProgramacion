09aa69ce3c1a24152c72a00c6476a75b32f267a0
AntoMei <antonio.meirosu444@gmail.com>
2021-10-27 10:35:02 +0200

    Finalizado libreria MatesPrueba y funciones Mates

---
+++ Tema 1/Tema2/MatesPruebas.java
@@ -0,0 +1,11 @@
+package Tema2;
+
+public class MatesPruebas {
+    public static void main(String[] args) {
+        System.out.println("Suma: 14 es igual a " +Mates.suma (7,7));
+        System.out.println("Resta: 8 es igual a " +Mates.resta (16,8));
+        System.out.println("Multiplicacion: 18 es igual a " +Mates.multiplicacion(2,9));
+        System.out.println("Division: 20 es igual a " +Mates.division (40,2));
+        System.out.println("areaCirculo (2) 12.566 es igual a " +Mates.areaCirculo(2));
+    }
+}
