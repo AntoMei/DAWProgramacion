31dbc920668ee61537acc13e20432344adc11059
AntoMei <antonio.meirosu444@gmail.com>
2021-10-29 09:25:47 +0200

    Cambios

--- Tema 2/Entrada.java
+++ Tema 2/Entrada.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
-
 public class Entrada {
     public static Integer getInteger(){
         Scanner entrada =  new Scanner(System.in);
         System.out.println("Escribe un número:");
-        entrada = 
+
+        if(entrada.hasNextInt()){
+            int x = entrada.nextInt();
+            System.out,println(""+x);
+        }
+        else {
+            System.out.println("Introduce un valor entero:");
+        }
+        entrada.close();
+    }
 }
--- Tema 2/EntradaPrueba.java
+++
@@ -1,4 +0,0 @@
-public class EntradaPrueba {
-    public static void main(String[] args) {
-    
-}
