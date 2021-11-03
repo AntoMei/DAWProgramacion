b17a27a5733093ab43488e2d1edbcdfee2390748
AntoMei <antonio.meirosu444@gmail.com>
2021-11-03 09:55:31 +0100

    Cmabios y Finalizado Ej25

--- Tema 1/Ejemplo25.java
+++ Tema 1/Ejemplo25.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
 public class Ejemplo25 {
     public static void main(String args[]){
-        Scanner inputValue = new Scanner(System.in);
-        System.out.println("Introduzca su factorial");
-        int x = inputValue.nextInt();
-        System.out.println(factorial(x));
+        int numero;
+        int factorial = 1;
+        Scanner inputValue = new Scanner (System.in);
+        
+        System.out.println("Introduce un número entero:");
+        numero = inputValue.nextInt();
+
+        if (numero>=0){
+            for (int i = 2; i <= numero; i++) {
+                factorial *=i;
+                
+            }
+            System.out.printf("El factorial de %d es %d", numero, factorial);
+        }else{
+            System.out.println("No se puede calcular el factorial");
+        }
         inputValue.close();
     }
-    public static int factorial(int numero){
-        int resultado = 1;
-        for (int i =1; i <= numero; i++){
-            resultado *=i;
-        }
-        return resultado;
-    }
-}
+} 
