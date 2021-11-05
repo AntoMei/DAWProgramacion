c33060aca97080d280c8f570b3e0728d497f39fa
AntoMei <antonio.meirosu444@gmail.com>
2021-11-05 09:19:33 +0100

    Cambios E01

--- Tema 1/Controles/Control02/E01.java
+++ Tema 1/Controles/Control02/E01.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class E01 {
     public static void main(String[] args) {
+        String nombre;
         Scanner lector = new Scanner(System.in);
         System.out.println("Ingrese su nombre:");
-        short nombre = lector.nextShort();
+        nombre = lector.nextLine();
         System.out.println("Hola Perico");
         lector.close();
     } 
