c07b8c0255608beddfb1d325c01633945628123d
AntoMei <antonio.meirosu444@gmail.com>
2021-11-05 10:28:25 +0100

    Finalizado E08

--- Tema 1/Controles/Control02/E08.java
+++ Tema 1/Controles/Control02/E08.java
@@ -1,7 +1,25 @@
 package Controles.Control02;
 
+import java.util.Scanner;
+
 public class E08 {
     public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+
+        double numero1; double numero2; double real; double decimal;
+
+        System.out.println("Ingrese un numero:");
+        numero1 = lector.nextDouble();
         
+        decimal = numero1 % 1;
+        real = numero1 -decimal;
+        System.out.printf(""+decimal,real);
+
+        System.out.println("Ingrese un numero:");
+        numero2 = lector.nextDouble();
+        decimal = numero2 % 1;
+        real = numero2 -decimal;
+        System.out.printf(""+decimal,real);
+        lector.close();
     }
 }
