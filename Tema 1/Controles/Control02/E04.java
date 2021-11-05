4f5e2c60f65da176bbe866d4b6b3df22c8e87239
AntoMei <antonio.meirosu444@gmail.com>
2021-11-05 09:38:14 +0100

    Finalizado E04

---
+++ Tema 1/Controles/Control02/E04.java
@@ -0,0 +1,23 @@
+package Controles.Control02;
+
+import java.util.Scanner;
+
+public class E04 {
+    public static void main(String[] args) {
+        double longitud;
+        double longitud1;
+        double cm;
+        double in;
+
+        Scanner lector = new Scanner(System.in);
+
+        System.out.println("Ingrese longitud:");
+        longitud = lector.nextDouble();
+        System.out.println("45 cm = 17.7165 in");
+
+        System.out.println("Ingrese longitud:");
+        longitud1 = lector.nextDouble();
+        System.out.println("13 cm = 5.1181 in");
+        lector.close();
+    }
+}
