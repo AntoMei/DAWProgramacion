344e93c47a22032ac61ee760f6f71decacbd68c3
AntoMei <antonio.meirosu444@gmail.com>
2021-11-15 08:28:09 +0100

    Modificado y Finalizado Ej potencia

--- Tema 2/EsPotencia.java
+++ Tema 2/EsPotencia.java
@@ -1,15 +1,14 @@
 public class EsPotencia {
     public static double potencia(double x, int n){
-        if (n == 0) return 1;
-        if (n == 1) return x;
-        if (n == 2) return x * x;
-        return x * (potencia(x, n-1));  
+        if (n == 0){ 
+            return 1;
+        }else{
+        return (x * potencia(x, n-1));  
+        }
     }
 
     public static void main(String[] args) {
-        double prueba1 = potencia(4, 2);
-        double prueba2 = potencia (8, 2);
-        double prueba3 = potencia (16, 2);
-        double prueba4 = potencia (32, 2);
+        System.out.println(potencia(3.0f, 2));
+        System.out.println(potencia(3.1f, 2));
     }
 }
