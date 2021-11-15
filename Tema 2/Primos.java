ffb08813a78917cf3f0d32e410bda79b61d1b40d
AntoMei <antonio.meirosu444@gmail.com>
2021-11-15 09:47:06 +0100

    Finalizado ej función primos

---
+++ Tema 2/Primos.java
@@ -0,0 +1,13 @@
+
+public class Primos {
+    public static void main(String[] args) {
+        for (int i = 3; i <=100; i++) {
+            boolean resultado = Primo.esPrimo(i);
+
+            if(resultado){
+                System.out.println(i);
+            
+        }
+    }
+}
+}
