eac19cae433e162f6e7af50fe7e6619a44f1565c
AntoMei <antonio.meirosu444@gmail.com>
2021-11-15 09:09:53 +0100

    Finalizado ej letraDNI

--- Tema 2/LetraDni.java
+++ Tema 2/LetraDni.java
@@ -1,7 +1,16 @@
 public class LetraDni {
-    public static int LetraDni(int[] numeros){
-    public static void main(String[] args) {
-        
+    public static int letraDni(int dni) {
+        char resultado = ' ';
+
+        char [] letras = {'T', 'R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+
+        int indice = dni%23;
+        resultado = letras[indice];
+
+        return resultado;
     }
-    
+    public static void main(String[] args) {
+        //Llamar a letraDNI con un entero directamente
+         System.out.println(letraDni(12345678));
+    }
 }
