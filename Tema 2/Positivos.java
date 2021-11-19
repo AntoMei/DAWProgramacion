366a28a0689a58f789538401f44528d45957b2bb
AntoMei <antonio.meirosu444@gmail.com>
2021-11-19 10:22:18 +0100

    Modificado y Finalizado Ej void Positivos

--- Tema 2/Positivos.java
+++ Tema 2/Positivos.java
@@ -3,23 +3,24 @@ import java.util.Scanner;
 public class Positivos {
     public static void positivos(int[] numeros){
         int resultado = 0;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < numeros.length; i++) {
             if (numeros[i]>=0){
                 resultado++;
             }
+        System.out.println(resultado);
         }
-        return;
     }
     public static void main(String[] args) {
-        int[] numeros;
-        int numero;
+        int[] numeros = new int [10];
         Scanner lector = new Scanner (System.in);
 
         //Empieza el programa
         System.out.println("Escribe 10 números");
-        numero = lector.nextInt();
-        lector.close();
 
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = lector.nextInt();  
+        }
+        lector.close();
         positivos(numeros);
     }
 }
