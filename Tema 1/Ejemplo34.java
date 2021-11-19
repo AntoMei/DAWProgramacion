0101e00e37b61478982f31e13ffb90d5f768c09e
AntoMei <antonio.meirosu444@gmail.com>
2021-11-19 09:00:12 +0100

    Modificado y Finalizado Ej34

--- Tema 1/Ejemplo34.java
+++ Tema 1/Ejemplo34.java
@@ -1,14 +1,35 @@
 import java.util.Scanner;
 public class Ejemplo34 {
     public static void main(String[] args) {
-        Scanner inputValue = new Scanner(System.in);
-        int numeroDNI;
-        int valorLetra;
-        String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+        String numeroDNI, numeroDNIsinLetra;
+        int valordeLetra = 0;
+        char[] anArray = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+        Scanner lector = new Scanner(System.in);
         
-        System.out.println("Introduce tu número de NIE sin letra:");
-        numeroDNI = inputValue.nextInt();
-        inputValue.close();
-        System.out.println("El numero de DNI es:"+numeroDNI+ "-" +letras.charAt(numeroDNI%23));
-    }
-}      
+        System.out.println("Dame los numero del NIE para verificar su letra:");
+        numeroDNI = lector.nextLine();
+
+        if(numeroDNI.length() == 8){
+
+            if(numeroDNI.charAt(0) == 'X'){
+                valordeLetra = 0;
+            } else if(numeroDNI.charAt(0) == 'Y'){
+                valordeLetra = 1;
+            } else if(numeroDNI.charAt(0) == 'Z'){
+                valordeLetra = 2;
+            } else {
+                System.out.println("Error");
+            }
+
+            numeroDNIsinLetra = numeroDNI.substring(1, numeroDNI.length());
+        
+            int numEntero = Integer.parseInt(numeroDNIsinLetra);
+            int numeroDNIconLetra = Integer.valueOf(String.valueOf(valordeLetra) + String.valueOf(numEntero));
+
+            System.out.println(numeroDNI + "" + anArray[numeroDNIconLetra%23]);
+        } else {
+            System.out.println("Erorr! El numero de digitos no es correcto!");
+        }
+        lector.close();
+    }    
+}
