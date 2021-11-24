072e62c1d2ead1a136f68cba02487204856b1d3c
AntoMei <antonio.meirosu444@gmail.com>
2021-11-24 09:27:24 +0100

    Cambios elimina array

---
+++ Tema3/Duplicados.java
@@ -0,0 +1,15 @@
+public class Duplicados {
+    public static void main(String[] args) {
+        int[]numeros ={1,2,4,4,7,8,8,9};
+
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros.length; j++) {
+                if(numeros[i] == numeros[j]){
+                    if(j!=i){
+                        System.out.println(i+" "+j);
+                    }
+                }
+            }
+        }
+    }
+}
--- Tema3/Elimina.java
+++ Tema3/Elimina.java
@@ -4,10 +4,10 @@ public class Elimina {
         int[] array = {1,2,3,4,5};
 
 
+
         
 
     
-
     }
     
 }
