5eb47c0e6a2092433a4882bb7f134d3c4d687d09
AntoMei <antonio.meirosu444@gmail.com>
2021-11-29 08:30:20 +0100

    Finalizado Ej Comunes java

---
+++ Tema3/Comunes.java
@@ -0,0 +1,14 @@
+public class Comunes {
+    public static void main(String[] args) {
+        int [] numeros1 = {1, 5, 7, 99, 4, 3};
+        int [] numeros2 = {6, 8, 99, 4, 2, 45};
+
+        for (int i = 0; i < numeros1.length; i++) {
+            for (int j = 0; j < numeros2.length; j++) {
+                if (numeros1[i] == numeros2[j]){
+                    System.out.println("Elemento común:"  + numeros1[i]);
+                }
+            }
+        }
+    }
+}
