d21c1ecc9a90379c0d42d6ab95acc922a5d77fc7
AntoMei <antonio.meirosu444@gmail.com>
2021-11-29 09:15:29 +0100

    Finalizado Ej eliminar java y modificado libreria utilidades

--- Tema3/Eliminar.java
+++ Tema3/Eliminar.java
@@ -5,17 +5,28 @@ public class Eliminar {
         //Utilidades.rellenaArray(elementos, 1, 20);
 
         int [] elementos = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int indice;
 
-        int indiceBorrar = 2;
+        indice = obtenerIndice(elementos);
+        eliminarElemento(elementos, indice);
+        Utilidades.mostrarArray(elementos);
+    }
+
+    private static int obtenerIndice(int[] elementos) {
+        int indice;
+        do{
+            indice = Utilidades.leerEntero("Índice de elemento a eliminar");
+        }while((indice < 0) || (indice > (elementos.length - 1)));
+        return indice;
+    }
+
+    private static void eliminarElemento(int[] elementos, int indiceBorrar) {
         int elementoBorrar = elementos[indiceBorrar];
 
         for (int i = indiceBorrar +1; i < elementos.length; i++) {
             elementos[i-1] = elementos[i];
             
         }
-
         elementos[elementos.length-1] = elementoBorrar;
-
-    }
-       
+    }      
 }
