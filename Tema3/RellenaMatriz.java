9525418e7a2fbabfccb2639d11bdb0d0bf5d7cd0
AntoMei <antonio.meirosu444@gmail.com>
2021-11-29 09:45:06 +0100

    Modificado Ej RellenaMatriz java

--- Tema3/RellenaMatriz.java
+++ Tema3/RellenaMatriz.java
@@ -1,14 +1,12 @@
-import java.util.Arrays;
-
 public class RellenaMatriz {
    public static void main(String[] args) {
-       
-    int[][] matriz = new int [10] [10];
-    int min = 0;
-    int max = 1;
-    Utilidades.rellenaMatriz(matriz, min, max);
-    
-    System.out.println("matriz: " + Arrays.deepToString(matriz));
+        int [][] matriz = new int[10][10];
+           
+        Utilidades.rellenaMatriz(matriz, 0, 1);
+
+        Utilidades.mostrarMatriz(matriz);
+        
+        System.out.println("Fin");
 
     }
 }
