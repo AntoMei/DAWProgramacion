c27df4bd7efa4fc96446626c0339b6eb781a120c
AntoMei <antonio.meirosu444@gmail.com>
2021-11-29 09:35:46 +0100

    Finalizado Ej Duplicados java

--- Tema3/Duplicados.java
+++ Tema3/Duplicados.java
@@ -1,15 +1,25 @@
-public class Duplicados {
-    public static void main(String[] args) {
-        int[]numeros ={1,2,4,4,7,8,8,9,9,4,5,9};
-        String duplicados = "";
+import java.util.Arrays;
 
-        for(int i=0;i<numeros.length-1;i++){
-            for(int j=i+1;j<numeros.length;j++){
-                if(numeros[i]==numeros[j]){
-                    duplicados += numeros[j] + " ";
-                    System.out.println("Elemento repetido: " +numeros[i]);
+public class Duplicados {
+        public static String duplicados(int[] numeros) {
+            String duplicados = "";
+            // Recorremos el array buscando el elemento iésimo
+            for (int i = 0; i < numeros.length - 1; i++) {
+                for (int j = i + 1; j < numeros.length; j++) {
+                    // Recorremos el array buscando a partir del elemento iésimo + 1
+                    if ((numeros[i] == numeros[j])) {
+                        duplicados += numeros[j] + " ";
+                        break;
+                    }
                 }
             }
-        }
+        return duplicados;
     }
-}
+    public static void main(String[] args) {
+        int[] numeros = new int[20];
+    
+        Utilidades.rellenaArray(numeros, -30, 30);
+        System.out.println("Array original: " + Arrays.toString(numeros));
+        System.out.println("Elementos duplicados: " + duplicados(numeros));
+    }
+} 
