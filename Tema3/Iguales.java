3ce7aaecad0a828fdfdf4ba8edc1ce5078d8b233
AntoMei <antonio.meirosu444@gmail.com>
2021-11-29 09:55:49 +0100

    Modificado Ej Iguales java

--- Tema3/Iguales.java
+++ Tema3/Iguales.java
@@ -1,13 +1,31 @@
-public class Iguales {
-    public static void main(String[] args) {
-        int intArray[] = {1,2,3};
-         
-        int cloneArray[] = intArray.clone();
+import java.util.Arrays;
 
-        System.out.println(intArray == cloneArray);
-         
-        for (int i = 0; i < cloneArray.length; i++) {
-            System.out.print(cloneArray+" ");
+public class Iguales {
+        public static String iguales(int[] original, int[] comparar) {
+            String iguales = "";
+            // Recorremos el original para buscar el elemento iésimo
+            for (int i = 0; i < original.length; i++) {
+                // Y ahora buscamos en el comparar el elemento iésimo
+                for (int j = 0; j < comparar.length; j++) {
+                    if (original[i] == comparar[j]) {
+                        iguales += original[i] + " ";
+                        break;
+                    }
+                }
+            }
+            return iguales;
         }
+    
+    public static void main(String[] args) {
+        int[] array1 = new int[10];
+        array1.clone();
+    
+        
+        Utilidades.rellenaArray(array1, -10, 10);
+    
+        // Antes de empezar a trabajar con el array lo imprimimos
+        System.out.println("Array : " + Arrays.toString(array1));
+    
+        System.out.println("Los elementos comunes son: " + iguales(array1, array1.clone()));
     }
 }
