7c42b1b92ec671428189a1406f2a103b93c21b24
AntoMei <antonio.meirosu444@gmail.com>
2021-12-01 09:09:40 +0100

    Finalizado Ej SumaIgual java

--- Tema3/SumaIgual.java
+++ Tema3/SumaIgual.java
@@ -1,31 +1,15 @@
-import java.util.Arrays;
-
 public class SumaIgual {
-    public static String sumaIgual(int[] numeros, int suma) {
-		String sumanIgual = "";
-		// Recorremos el array
-		for (int i = 0; i < numeros.length; i++) {
-			for (int j = i + 1; j < numeros.length; j++) {
-				// Recorremos el array sumando a partir del elemento iésimo
-				if ((numeros[i] + numeros[j]) == suma) {
-					sumanIgual += "[" + numeros[i] + " + " + numeros[j] + "] ";
-					break;
-				}
+   public static void main(String[] args) {
+	   int [] vector = {1,2,3,4,5,6,7,8,9,10};
+	   int numero = 5;
+
+	   for (int i = 0; i < vector.length; i++) {
+		   for (int j = i + 1 ; j < vector.length; j++) {
+			   int suma = vector[i] + vector[j];
+			   if (suma == numero){
+				   System.out.println(vector[i] + ", "+ vector[j]);
+			   }
 			}
 		}
-		return sumanIgual;
 	}
-
-	public static void main(String[] args) {
-		int[] numeros = new int[10];
-		int num;
-		Utilidades.rellenaArray(numeros, 10, -10);
-
-		// Antes de empezar a trabajar con el array lo imprimimos
-		System.out.println("Array : " + Arrays.toString(numeros));
-
-		num = Utilidades.leerEntero("Introduce un número entero para buscar");
-
-		System.out.println("Los números siguientes: " + sumaIgual(numeros, num) + " suman " + num);
-    }
 }
