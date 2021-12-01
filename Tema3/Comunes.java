131b7662dad84d4263d838a91c0d9a11975f84cb
AntoMei <antonio.meirosu444@gmail.com>
2021-12-01 08:29:54 +0100

    Finalizado Ej Comunes java

--- Tema3/Comunes.java
+++ Tema3/Comunes.java
@@ -3,6 +3,9 @@ public class Comunes {
         int [] numeros1 = {1, 5, 7, 99, 4, 3};
         int [] numeros2 = {6, 8, 99, 4, 2, 45};
 
+        System.out.println("===============");
+        System.out.println("==  Repetidos ==");
+        System.out.println("===============");
         for (int i = 0; i < numeros1.length; i++) {
             for (int j = 0; j < numeros2.length; j++) {
                 if (numeros1[i] == numeros2[j]){
