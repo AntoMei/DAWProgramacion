01ffd418b2be5201ec8c38146a8418b43d51bc43
AntoMei <antonio.meirosu444@gmail.com>
2021-12-10 09:20:59 +0100

    Finalizado Ej RellenaMatriz java

--- Tema3/RellenaMatriz.java
+++ Tema3/RellenaMatriz.java
@@ -1,12 +1,23 @@
 public class RellenaMatriz {
-   public static void main(String[] args) {
-        int [][] matriz = new int[10][10];
-           
-        Utilidades.rellenaMatriz(matriz, 0, 1);
-
-        Utilidades.mostrarMatriz(matriz);
+	public static void main(String[] args) {
+        int[][] matriz = new int[10][10];
+        int min = 0;
+        int max = 1;
         
-        System.out.println("Fin");
+        int filas = matriz.length;
+		int columnas = matriz[0].length;
+		for (int i = 0; i < filas; i++){
+			for (int j = 0; j < columnas; j++){
+			    matriz[i][j] = (int) (Math.random() * (max - min + 1) + min);
+			}
+		}
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 }
