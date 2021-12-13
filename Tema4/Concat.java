f414f8e9127aaad0f4f4b8421fb25c7aab9ef55f
AntoMei <antonio.meirosu444@gmail.com>
2021-12-13 09:01:55 +0100

    Finalizado Ej String Concat java

---
+++ Tema4/Concat.java
@@ -0,0 +1,12 @@
+public class Concat {
+    public static void main(String[] args) {
+        String cadena1 = "aaaa";
+        String cadena2 = "bbbb";
+
+        String cadena3 = cadena1.concat(cadena2);
+
+        System.out.println(cadena3);
+
+    }
+    
+}
