96d13db4ec40c5448f14317ad4156744331e58ef
AntoMei <antonio.meirosu444@gmail.com>
2021-12-13 09:21:03 +0100

    Añadir libreria utilidades y finalizado el ejercicio

--- Tema4/Concat.java
+++ Tema4/Concat.java
@@ -1,7 +1,7 @@
 public class Concat {
     public static void main(String[] args) {
-        String cadena1 = "aaaa";
-        String cadena2 = "bbbb";
+        String cadena1 = Utilidades.leerCadena("Introduce cadena1");
+		String cadena2 = Utilidades.leerCadena("Introduce cadena2");
 
         String cadena3 = cadena1.concat(cadena2);
 
