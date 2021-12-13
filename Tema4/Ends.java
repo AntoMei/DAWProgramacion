269e0285bd6b2cf3cca1d1171ddce0779ec8aa62
AntoMei <antonio.meirosu444@gmail.com>
2021-12-13 09:18:45 +0100

    Finalizado Ej String Ends java

--- Tema4/Ends.java
+++ Tema4/Ends.java
@@ -1,3 +1,8 @@
 public class Ends {
-    
+    public static void main(String[] args) {
+        String cadena1 = Utilidades.leerCadena("Introduce cadena1");
+		String cadena2 = Utilidades.leerCadena("Introduce cadena2");
+
+        System.out.println(cadena1.endsWith(cadena2));
+    }
 }
