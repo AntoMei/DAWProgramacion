2512ed1810ef6aeb993c12eb8757e1ea0c343ae8
AntoMei <antonio.meirosu444@gmail.com>
2021-12-13 09:12:01 +0100

    Finalizado Ej String Contains java

---
+++ Tema4/Contains.java
@@ -0,0 +1,9 @@
+public class Contains {
+    public static void main(String[] args) {
+        String cadena1 = "Hola";
+        String cadena2 = "Hola que tal";
+
+        System.out.println(cadena1.contains("Ho"));
+        System.out.println(cadena2.contains("que"));
+    }
+}
