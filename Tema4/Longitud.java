b27df0a26b46764560af6f6982ca9ea3ee0735d0
AntoMei <antonio.meirosu444@gmail.com>
2021-12-15 09:55:16 +0100

    Finalizado Ej String Longitud java

--- Tema4/Longitud.java
+++ Tema4/Longitud.java
@@ -1,15 +1,19 @@
 public class Longitud {
-    public static void main(String[] args) {
-        String cadena = Utilidades.leerCadena("Introduzca la cadena:");
-        String palabras [] = Utilidades.dividirEnPalabras(cadena);
-        int contador = 0;
-        int longitud = Utilidades.leerEntero("mensaje");
 
+    public static int cuantasDeLongitud(String[] palabras, int longitud) {
+        int contador = 0;
         for (int i = 0; i < palabras.length; i++) {
             if (palabras[i].length() == longitud){
                 contador++;
             }
-        System.out.println("La cadena contiene" + palabras+ "de"+ longitud);
         }
+        return contador;
     }
+    public static void main(String[] args) {
+        String cadena = Utilidades.leerCadena("Introduzca la cadena:");
+        String palabras [] = Utilidades.dividirEnPalabras(cadena);
+        int longitud = Utilidades.leerEntero("mensaje");
+
+        System.out.printf("La cadena contiene %d palabras de longitud %d", cuantasDeLongitud(palabras, longitud), longitud);
+	}
 }
