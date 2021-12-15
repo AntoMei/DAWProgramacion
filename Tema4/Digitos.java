c1ef662aa4979c1f3b235d8d3911a87b26c55a05
AntoMei <antonio.meirosu444@gmail.com>
2021-12-15 08:49:24 +0100

    Ej Digitos String no terminado

---
+++ Tema4/Digitos.java
@@ -0,0 +1,17 @@
+public class Digitos {
+
+    public static int cuantodDigitos(String cadena){
+        int contador = 0;
+        for (int i = 0; i < cadena.length(); i++) {
+            if(Character.isDigit(cadena.charAt(i))){
+                contador++;
+            }
+        }
+        return contador;
+    }
+    public static void main(String[] args) {
+        String cadena = Utilidades.leerCadena("Introduzca la cadena:");
+
+        System.out.println(cuantodDigitos(cadena));
+    }
+}
