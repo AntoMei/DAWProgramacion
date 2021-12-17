0dd899fad7a9441694512a8f6757c865329669a2
AntoMei <antonio.meirosu444@gmail.com>
2021-12-17 08:46:17 +0100

    No terminado Ej Dígitos

--- Tema4/Digitos.java
+++ Tema4/Digitos.java
@@ -1,17 +1,16 @@
 public class Digitos {
 
-    public static int cuantosDigitos(String cadena){
-        int contador = 0;
-        for (int i = 0; i < cadena.length(); i++) {
-            if(Character.isDigit(cadena.charAt(i))){
-                contador++;
-            }
-        }
-        return contador;
-    }
     public static void main(String[] args) {
         String cadena = Utilidades.leerCadena("Introduzca la cadena:");
+        char [] cadena_num = cadena.toCharArray();
+        String n = "";
 
-        System.out.println(cuantosDigitos(cadena));
+        for (int i = 0; i < cadena_num.length; i++) {
+            if(Character.isDigit(cadena_num[i])){
+                n += cadena_num[i];
+            }
+        }
+
+        System.out.println("Números: "+n);
     }
 }
