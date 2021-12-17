f527feb2937040b9aae9fa364464bcd75e266fb4
AntoMei <antonio.meirosu444@gmail.com>
2021-12-17 10:20:52 +0100

    No terminado Ej Siglas

---
+++ Tema4/Siglas.java
@@ -0,0 +1,13 @@
+public class Siglas {
+    public static String [] palabras(String cadena){
+        String palabras [] = cadena.split(" ");
+        return palabras;
+    }
+    public static String siglas(String[] palabras){
+        String siglas = "";
+            
+        }
+    }
+    public static void main(String[] args) {
+        String cadena = Utilidades.leerCadena("Introduzca la cadena:");
+
