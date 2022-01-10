7f4d82e0cb0b3525a1dba065c101dbcc3a2cf6a0
AntoMei <antonio.meirosu444@gmail.com>
2022-01-10 09:42:27 +0100

    Terminado Ej Alfabética

---
+++ Tema4/Alfabetica.java
@@ -0,0 +1,23 @@
+public class Alfabetica {
+    public static boolean esAlfabetica (String cadena) {
+
+        for (int i = 1; i < cadena.length(); i++) {
+            if(cadena.charAt(i) < cadena.charAt(i-1)){
+                return false;
+            }
+        }
+        return true;
+        }
+
+    public static void main(String[] args) {
+        String palabra = Utilidades.leerCadena("Introduce una palabra");
+		if ( palabra.length() > 0) {
+			if(esAlfabetica(palabra)) {
+				System.out.println("La palabra es alfabética");
+			}
+			else {
+				System.out.println("La palabra no es alfabética");
+			}
+        }
+    }
+}
