bffc557205395dee74a9267453223cc888f4c219
AntoMei <antonio.meirosu444@gmail.com>
2022-01-12 11:59:28 +0100

    Creado la clase y los metodos pero no terminado

--- Tema5/Alumno.java
+++ Tema5/Alumno.java
@@ -1,3 +1,42 @@
 public class Alumno {
     
+    private int id;
+    private static String nombre;
+    private int grupo;
+    private static int numAlumnos = 0;
+
+    static void nuevoAlumno() {
+        numAlumnos++;
+    }
+
+    public Alumno(String pepito, int a) {
+        nombre = pepito;
+        id = a;
+        nuevoAlumno();
+    }
+
+    public Alumno(int b, String juanito, int cfsj ) {
+        id = b;
+        nombre = juanito;
+        grupo = cfsj;  
+        nuevoAlumno();
+    }
+
+    static void preguntarDuda() {
+        System.out.println(nombre);
+    }
+
+    static void inprimirInforme(){
+        System.out.println(numAlumnos);
+    }
+
+    static void imprimirInformeAlumnos(){
+    }
+
+
+    public static void main(String[] args) {
+        Alumno a1, a2;
+        a2 = new Alumno(nombre);
+        Alumno.preguntarDuda();
+    }
 }
