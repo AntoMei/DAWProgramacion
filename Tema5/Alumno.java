f52dea6630f4f420cf06dbff7f5d28cb255970e7
AntoMei <antonio.meirosu444@gmail.com>
2022-01-14 08:53:06 +0100

    Finalizado Ej Alumno y AlumnoTest

--- Tema5/Alumno.java
+++ Tema5/Alumno.java
@@ -1,41 +1,48 @@
-public class Alumno {
+class Alumno {
+
+    //Zona de atributos de clase
+
+    private static int totalAlumnos = 0;
+
+    //Zona de atributos
     
     private int id;
     private String nombre;
-    private int grupo;
+    private String grupo;
     private static int numAlumnos = 0;
-    
-    static void nuevoAlumno() {
-        numAlumnos++;
+
+    //Zona de métodos
+
+    public Alumno(int identificador, String nombreAlumno) {
+      id = identificador;
+      nombre = nombreAlumno;
+      grupo = "No asignado"; 
+
+      totalAlumnos++;
     }
 
-    public Alumno(String pepito, int a) {
-        nombre = pepito;
-        id = a;
-        nuevoAlumno();
+    public Alumno(int identificador, String nombreAlumno, String grupoAlumno) {
+        id = identificador;
+        nombre = nombreAlumno;
+        grupo = grupoAlumno;
+
+        totalAlumnos++;
     }
 
-    public Alumno(int b, String juanito, int cfsj ) {
-        id = b;
-        nombre = juanito;
-        grupo = cfsj;  
-        nuevoAlumno();
-
-
+    public void preguntarDuda(){
+        System.out.println(nombre + " levanta la mano");
     }
 
-    static void preguntarDuda() {
-        System.out.println(nombre);
+    public static void imprimirInforme(){
+        System.out.println("Total de alumnos: " +totalAlumnos);
     }
 
-    static void inprimirInforme(){
-        System.out.println(numAlumnos);
-    }
-
-    static void imprimirInformeAlumnos(){
-    }
-    
-    public static void main(String[] args) {
-     
+    public static void imprimirInformeAlumnos(Alumno [] alumnos){
+        System.out.println("Nombre\t\t\tGrupo");
+        for(Alumno alumno : alumnos){
+            System.out.println(alumno.nombre + "\t\t\t" + alumno.grupo);
+            //\t expresa un tabulador, imprime un tabulador
+            ///n expresa un salto de línea, imprime un salto de línea
+        }
     }
 }
