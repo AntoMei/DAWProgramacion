5e42a1d9cbd78040571a73eff82ade5b2bc85f9a
AntoMei <antonio.meirosu444@gmail.com>
2022-01-19 10:29:23 +0100

    Finalizado Ej Silla y SillaTest

--- Tema5/Silla.java
+++ Tema5/Silla.java
@@ -1,12 +1,62 @@
+import java.awt.Point;
+
 public class Silla {
 
-    private int color;
-    private int numeroPatas;
-    private boolean respaldo;
-    private int comodidad;
+    public Silla() {
+        //Aquí generar silla aleatoria
+    }
+    public Silla(String color, int numeroPatas, boolean tieneRespaldo, int comodidad, String material,
+            Point ubicacion) {
+        this.color = color;
+        this.numeroPatas = numeroPatas;
+        this.tieneRespaldo = tieneRespaldo;
+        this.comodidad = comodidad;
+        this.material = material;
+        this.ubicacion = ubicacion;
+    }
+
+    //Atributos de objetos
+    private String color;
+    private int numeroPatas; //0-100
+    private boolean tieneRespaldo;
+    private int comodidad; //0-10
     private String material;
+    private Point ubicacion;
 
-    
-
-    
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public int getNumeroPatas() {
+        return numeroPatas;
+    }
+    public void setNumeroPatas(int numeroPatas) {
+        this.numeroPatas = numeroPatas;
+    }
+    public boolean isTieneRespaldo() {
+        return tieneRespaldo;
+    }
+    public void setTieneRespaldo(boolean tieneRespaldo) {
+        this.tieneRespaldo = tieneRespaldo;
+    }
+    public int getComodidad() {
+        return comodidad;
+    }
+    public void setComodidad(int comodidad) {
+        this.comodidad = comodidad;
+    }
+    public String getMaterial() {
+        return material;
+    }
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+    public Point getUbicacion() {
+        return ubicacion;
+    }
+    public void setUbicacion(Point ubicacion) {
+        this.ubicacion = ubicacion;
+    }
 }
--- Tema5/SillaTest.java
+++ Tema5/SillaTest.java
@@ -1,5 +1,12 @@
+import java.awt.Point;
+
 public class SillaTest {
     public static void main(String[] args) {
-        
+        Silla s1 = new Silla();
+        s1.setColor("rojo");
+        s1.setComodidad(2);
+        s1.setNumeroPatas(4);
+        s1.setTieneRespaldo(true);
+        s1.setUbicacion(new Point(0,1));
     }
 }
