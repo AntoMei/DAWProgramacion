3116775c347063810acfe169004aac850f80f118
AntoMei <antonio.meirosu444@gmail.com>
2022-01-21 10:16:43 +0100

    Finalizado Ej TicketMachine y TicketMachineTest

---
+++ Tema5/Ejercicios/TicketMachineTest.java
@@ -0,0 +1,14 @@
+package Ejercicios;
+
+public class TicketMachineTest {
+    public static void main(String[] args) {
+        TicketMachine maquina = new TicketMachine(100);
+        maquina.prompt();
+        maquina.insertMoney(200);
+        maquina.insertMoney(100);
+        maquina.printTicket();
+        maquina.showPrice();
+        maquina.empty();
+        maquina.showPrice();  
+    }
+}
