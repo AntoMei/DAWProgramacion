2d1b42e4180046e175db14af72ff76011d13c13d
AntoMei <90893283+AntoMei@users.noreply.github.com>
2022-02-04 15:44:06 +0100

    No finalizado, falta método conectar ordenador

--- Controles/Ordenador.java
+++ Controles/Ordenador.java
@@ -4,6 +4,7 @@ public class Ordenador {
   private String color;
   private Teclado teclado;
 
+  //Contructores
   public Ordenador(){
     this.formato = "sobremesa";
     this.color ="azul";
@@ -11,10 +12,42 @@ public class Ordenador {
   }
 
   public Ordenador(String formato, String color, Teclado teclado){
-    this.formato = formato;
+    this.formato = Ordenador.comprobarFormatoOrdenador(formato);
     this.color = color;
     this.teclado = teclado;
   }
   
+  //Getters y Setters
+  public String getFormato() {
+    return formato;
+  }
+
+  public void setFormato(String formato) {
+    this.formato = formato;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public Teclado getTeclado() {
+    return teclado;
+  }
+
+  public void setTeclado(Teclado teclado) {
+    this.teclado = teclado;
+  }
+
+  //Método comprobar formato del Ordenador
+  private static String comprobarFormatoOrdenador(String formato){
+    if (formato == "sobremesa" || formato == "portátil"){
+    return formato;
+    }else{
+      return formato = "sobremesa";
+    }
   }
 }
