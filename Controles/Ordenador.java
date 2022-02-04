7f16c5cad94eaacd0f17a914ce00de2f48087150
AntoMei <90893283+AntoMei@users.noreply.github.com>
2022-02-04 15:29:18 +0100

    No finalizado

--- Controles/Ordenador.java
+++ Controles/Ordenador.java
@@ -3,4 +3,18 @@ public class Ordenador {
   private String formato;
   private String color;
   private Teclado teclado;
+
+  public Ordenador(){
+    this.formato = "sobremesa";
+    this.color ="azul";
+    teclado = new Teclado();
+  }
+
+  public Ordenador(String formato, String color, Teclado teclado){
+    this.formato = formato;
+    this.color = color;
+    this.teclado = teclado;
+  }
+  
+  }
 }
