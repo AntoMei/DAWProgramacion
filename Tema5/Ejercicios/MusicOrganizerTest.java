6154551ee0ec13059faa4cfdbfe961de550d6f1e
AntoMei <antonio.meirosu444@gmail.com>
2022-02-09 09:12:34 +0100

    Finalizado Test MusicOrganizer

--- Tema5/Ejercicios/MusicOrganizerTest.java
+++ Tema5/Ejercicios/MusicOrganizerTest.java
@@ -3,7 +3,6 @@ package Ejercicios;
 public class MusicOrganizerTest {
     public static void main(String[] args) {
         MusicOrganizer m1 = new MusicOrganizer();
-        
-    
+        m1.addFile("Texto");
     }
 }
