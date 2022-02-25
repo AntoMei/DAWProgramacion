aee17b74fc17d93fdf2fafe2724dfee150669628
AntoMei <antonio.meirosu444@gmail.com>
2022-02-25 10:35:08 +0100

    No terminado

--- Tema5/Ejercicios/CutreCloud/Media.java
+++ Tema5/Ejercicios/CutreCloud/Media.java
@@ -1,31 +1,21 @@
 package Ejercicios.CutreCloud;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Media {
-
     private int id;
     private String nombre;
     private String contenido;
     private MediaType tipo;
-    private Usuario usuario_id;
+    private Usuario usuario;
 
     public static ArrayList<Media> list = new ArrayList<>();
+    private static int idGenerator = 0;
 
-    public Media(int id, String nombre, String contenido, MediaType tipo, Usuario usuario_id) {
-        this.id = id;
-        this.nombre = nombre;
-        this.contenido = contenido;
-        this.tipo = tipo;
-        this.usuario_id = usuario_id;
-    }
-
+    
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getNombre() {
@@ -52,12 +42,41 @@ public class Media {
         this.tipo = tipo;
     }
 
-    public Usuario getUsuario_id() {
-        return usuario_id;
+    public Usuario getUsuario() {
+        return usuario;
     }
 
-    public void setUsuario_id(Usuario usuario_id) {
-        this.usuario_id = usuario_id;
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Media(String nombre, String contenido, MediaType tipo, Usuario usuario) {
+        
+        this.contenido = contenido;
+        this.tipo = tipo;
+        this.usuario = usuario;
+
+        if (this.isNameAvailable(nombre)){
+            this.id = idGenerator++;
+            this.nombre = nombre;
+            list.add(this);
+        }else{
+            this.id = -1;
+            this.nombre = "Nombre duplicado: " + nombre;
+        }
+    }
+
+    private boolean isNameAvailable(String name){
+        boolean resultado = true;
+
+        for (Media media : list) {
+            if(name.equals(media.getNombre())){
+                resultado = false;
+                break;
+            }
+        }
+
+        return resultado;
     }
 
     //Método eliminar un media por nombre fichero.
@@ -74,10 +93,11 @@ public class Media {
     //Método eliminar todos media de un tipo concreto.
 
     public void eliminarTodosMedias(MediaType tipo){
-        for (Media item : list) {
-            if (item.getTipo().equals(tipo)) {
-                list.remove(item);
-                break;
+        Iterator<Media> i = list.iterator();
+        while(i.hasNext()){
+            Media media = i.next();
+            if (media.getTipo().equals(tipo)) {
+                i.remove();
             }
         }
     }
