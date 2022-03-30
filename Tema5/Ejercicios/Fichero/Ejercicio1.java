8334eab4f60813ee27eaea8471a3f44cc6236aa0
AntoMei <antonio.meirosu444@gmail.com>
2022-03-30 10:17:10 +0200

    No terminado

--- Tema5/Ejercicios/Fichero/Ejercicio1.java
+++ Tema5/Ejercicios/Fichero/Ejercicio1.java
@@ -1,23 +1,47 @@
 package Ejercicios.Fichero;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Ejercicio1 {
-    public static void main(String[] args) throws IOException{
-
-        System.out.println("Escribe la ruta: ");
-        String entrada = new BufferedReader(new InputStreamReader(System.in)).readLine();
-
-        File fichero = new File(entrada);
         
-        System.out.println("Lista de ficheros y directorios del directorio: " + entrada);
+    public static void root(){
+        
+        File fichero = File.listRoots()[0]; 
+        File contenido[] = fichero.listFiles();
+        
+        int contador = 1;
 
+        System.out.println("Lista de ficheros y directorios del directorio: ");          
         System.out.println("---------------------------------------------");
-            for (String item : fichero.list()) {
-                System.out.println(item);
+        
+        if (fichero.getParent() != null) {
+            System.out.println(fichero.getParent());
+        }else{
+            System.out.println("0.- Directorio Padre");
+        }
+
+        for (int j = 0; j < contenido.length; j++) {
+            if (contenido[j].isDirectory()) {
+                System.out.println(contador + ".- " + contenido[j] + " <Directorio>");
+            }else if (contenido[j].isFile()) {
+                System.out.println(contador + ".- " + contenido[j] + " " + contenido[j].length() + " bytes");
             }
+            contador++;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{ 
+
+        Scanner teclado = new Scanner(System.in);
+
+        int opcion;
+
+        System.out.println("Lista de ficheros y directorios del directorio: ");          
+        System.out.println("---------------------------------------------");
+        root();
+        System.out.println("Introduce una opcion (-1 para salir): ");
+        opcion = teclado.nextInt(); 
     }
 }
