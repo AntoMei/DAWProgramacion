cc236b3636896cff5c350d4020e9251479ee2ae2
AntoMei <antonio.meirosu444@gmail.com>
2022-04-01 09:27:40 +0200

    Finalizado Ejercicio 1

--- Tema5/Ejercicios/Fichero/Ejercicio1.java
+++ Tema5/Ejercicios/Fichero/Ejercicio1.java
@@ -3,45 +3,80 @@ package Ejercicios.Fichero;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
-
 public class Ejercicio1 {
-        
-    public static void root(){
-        
-        File fichero = File.listRoots()[0]; 
-        File contenido[] = fichero.listFiles();
-        
+
+   private static void imprimirFichero(File e, int id) {
+
+        //No imprime los ficheros/directorios ocultos
+
+       if (!e.isHidden()) {
+           if (e.isFile()) {
+               System.out.println(id + ".- " + e.getName() + " " + e.length());
+           }
+           if (e.isDirectory()){
+               System.out.println(id + ".- " + e.getName() + " <Directorio>");
+           }
+       }
+   }
+   private static void imprimirDirectorio(File f) throws IOException {
+
         int contador = 1;
 
-        System.out.println("Lista de ficheros y directorios del directorio: ");          
-        System.out.println("---------------------------------------------");
-        
-        if (fichero.getParent() != null) {
-            System.out.println(fichero.getParent());
-        }else{
-            System.out.println("0.- Directorio Padre");
-        }
+            if (f.isDirectory()) {
+                System.out.println("Lista de ficheros y directorios del directorio: " + f.getCanonicalPath());
+                System.out.println("---------------------------------------------------");
 
-        for (int j = 0; j < contenido.length; j++) {
-            if (contenido[j].isDirectory()) {
-                System.out.println(contador + ".- " + contenido[j] + " <Directorio>");
-            }else if (contenido[j].isFile()) {
-                System.out.println(contador + ".- " + contenido[j] + " " + contenido[j].length() + " bytes");
+                if (f.getParent() != null) {
+                    System.out.println("0.- Directorio padre"); 
+                }
+                
+                for (File e : f.listFiles()){
+                    imprimirFichero(e, contador);
+                    contador++;
+                }
+            } else {
+                System.out.println("No es un directorio");
             }
-            contador++;
+
+        } else {
+            System.out.println("No existe el directorio");
         }
     }
-
-    public static void main(String[] args) throws IOException{ 
-
+    
+    private static int leerOpcion() {
         Scanner teclado = new Scanner(System.in);
+        String entradaUsuario = "";
+        int resultado = 0;
+    
+        System.out.println("Introduce una opcion (-1 para salir):");
+    
+        entradaUsuario = teclado.nextLine();
+    
+        resultado = Integer.parseInt(entradaUsuario); //Posible error en parse.
+    
+        return resultado;
+    }
 
+    public static void main(String[] args) throws IOException {
+
+        File actualFile  = File.listRoots()[0]; //Empezamos con el fichero raíz.
         int opcion;
 
-        System.out.println("Lista de ficheros y directorios del directorio: ");          
-        System.out.println("---------------------------------------------");
-        root();
-        System.out.println("Introduce una opcion (-1 para salir): ");
-        opcion = teclado.nextInt(); 
+        do {
+            imprimirDirectorio(actualFile);
+            opcion = leerOpcion();
+
+            if (opcion >= 1 && opcion <= actualFile.list().length) {
+
+                File nextFile = actualFile.listFiles()[opcion-1];
+
+                if (nextFile.isDirectory() && nextFile.canRead()) {
+                    actualFile = nextFile;
+                    }
+                }
+            else if (actualFile.getParent() != null && opcion  == 0){
+                actualFile = actualFile.getParentFile();
+            }
+        } while (opcion != -1);
     }
 }
