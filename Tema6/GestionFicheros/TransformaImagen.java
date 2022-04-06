584430dc24957390be67c6b759cae0e4d315fea7
AntoMei <antonio.meirosu444@gmail.com>
2022-04-06 08:30:54 +0200

    Finalizado Ejercicio TransformaImagen

--- Tema6/GestionFicheros/TransformaImagen.java
+++ Tema6/GestionFicheros/TransformaImagen.java
@@ -2,61 +2,101 @@ package GestionFicheros;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import java.awt.Graphics2D;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class TransformaImagen {
-
     File f = null;
-
-    public TransformaImagen(File fEnt) {
-
-        // Control de existencia del fichero y control de la extensión .bmp (sacar
-
-        // mensajes de error)
-        if (fEnt.exists()== false) {
-            System.out.println("El fichero no existe");
-            
+    public TransformaImagen(File fEnt) {        
+        if(!fEnt.getName().substring(fEnt.getName().indexOf(".")).equals(".bmp")) {
+                
         }
-
+        else {
+            f = fEnt;
+        }
     }
 
     public void transformaNegativo() throws IOException {
+        FileInputStream reader = new FileInputStream(this.f);
+        File fOut = new File("./fichero_n.bmp");
+        FileOutputStream writer = new FileOutputStream(fOut);
 
-        // Transformar a negativo y guardar como *_n.bmp
-
-        FileInputStream fImg_in = new FileInputStream(f);
-        FileOutputStream fImg_ou = new FileOutputStream(f + "_n.bmp");
-
-        byte[] buffer = new byte[54];
-        int n = fImg_in.read(buffer);
+        byte[] cabecera = new byte[54];
         
+        int c = 0;
 
- 
+        reader.read(cabecera);
+        writer.write(cabecera);
+
+        c = reader.read();
+        while(c != -1) {
+            writer.write(((char)(255 - c)));
+            c = reader.read();
+        }
+
+        reader.close();
+        writer.close();
     }
 
     public void transformaOscuro() throws IOException {
+        FileInputStream reader = new FileInputStream(this.f);
+        File fOut = new File("./fichero_o.bmp");
+        FileOutputStream writer = new FileOutputStream(fOut);
 
-        // Transformar a una imagen más oscura y guardar como *_o.bmp
-
+        byte[] cabecera = new byte[54];
         
+        int c = 0;
 
+        reader.read(cabecera);
+        writer.write(cabecera);
+
+        c = reader.read();
+        while(c != -1) {
+            writer.write((char)c/2);
+            c = reader.read();
+        }
+
+        reader.close();
+        writer.close();
     }
+    public void transformaNegroBlanco() throws IOException {
+        FileInputStream reader = new FileInputStream(this.f);
+        File fOut = new File("./fichero_nb.bmp");
+        FileOutputStream writer = new FileOutputStream(fOut);
 
-    
-
-    public void transformaBlancoNegro() throws IOException {
-
-        // Transformar a una imagen en blanco y negro y guardar como *_bn.bmp
-
-    }
-
-    private String getNombreSinExtension() {
-
-        //Devuelve el nombre del archivo f sin extensión
-
+        byte[] cabecera = new byte[54];
         
+        int r = 0;
+        int g = 0;
+        int b = 0;
 
+        reader.read(cabecera);
+        writer.write(cabecera);
+
+        r = reader.read();
+        while(r != -1) {
+            g = reader.read();
+            b = reader.read();
+
+            int media = (r+g+b)/3;
+
+            writer.write((char)media);
+            writer.write((char)media);
+            writer.write((char)media);
+
+            r = reader.read();
+        }
+
+        reader.close();
+        writer.close();
+        
     }
-
 }
---
+++ Tema6/Mezcla/Mezcla.java
@@ -0,0 +1,5 @@
+package Mezcla;
+
+public class Mezcla {
+    
+}
