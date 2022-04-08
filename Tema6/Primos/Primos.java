bea389948d95cc62f1f920c635ab3fc6a4ccf1c1
AntoMei <antonio.meirosu444@gmail.com>
2022-04-08 08:57:04 +0200

    Finalizado Ejercicio Primos

---
+++ Tema6/Primos/Primos.java
@@ -0,0 +1,39 @@
+package Primos;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Primos {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("Tema6/Primos/primos.dat"));
+
+            for (int i = 1; i < 501; i++) {
+                if (esPrimo(i)) {
+                    bw.write(String.valueOf(i) + "\n");
+                }
+            }
+            bw.close();;
+
+        } catch (IOException e) {
+            System.out.println("Error");
+        } 
+    }
+
+    public static boolean esPrimo(int numero){
+        
+        if (numero == 0 || numero == 1 || numero == 4) {
+            return false;
+        }
+        for (int i = 2; i < numero / 2; i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
