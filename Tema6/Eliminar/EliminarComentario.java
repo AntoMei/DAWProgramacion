52f4f6cd613224994a59adfab6677f3407cf47ab
AntoMei <antonio.meirosu444@gmail.com>
2022-04-13 09:33:02 +0200

    Finalizado primera version de eliminar comentarios

--- Tema6/Eliminar/EliminarComentario.java
+++ Tema6/Eliminar/EliminarComentario.java
@@ -1,9 +1,29 @@
 package Eliminar;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class EliminarComentario {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+            BufferedReader br = new BufferedReader(new FileReader("Tema6/Eliminar/fichero.txt"));
+            String linea = "";
+            String inicio;
+            int contador = 0;
+
+            while ((linea = br.readLine()) != null) {
+                    linea = linea.replaceAll("\\s", "");
+
+                    if (linea.length() > 1) {
+                        inicio = linea.substring(0, 2);
         
-        
+                    if (inicio.equals("//")) {
+                        contador++;
+                    }
+                }
+            }
+            System.out.println(contador);
+            br.close();
+        }
     }
-    
-}
