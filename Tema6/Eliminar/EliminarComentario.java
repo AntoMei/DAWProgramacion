813deb14c09d4586d1e1cb97e41395084b9a9467
AntoMei <90893283+AntoMei@users.noreply.github.com>
2022-04-26 18:18:57 +0200

    No terminado

--- Tema6/Eliminar/EliminarComentario.java
+++ Tema6/Eliminar/EliminarComentario.java
@@ -3,27 +3,42 @@ package Eliminar;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class EliminarComentario {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new FileReader("Tema6/Eliminar/fichero.txt"));
+        PrintWriter pw = new PrintWriter(("Tema6/Eliminar/fichero2.txt"));
+
         String linea = "";
+        String liniaSinEspacios;
         String inicio;
-        int contador = 0;
 
         while ((linea = br.readLine()) != null) {
-                linea = linea.replaceAll("\\s", "");
+                liniaSinEspacios = linea.replaceAll("\\s", "");
 
-                if (linea.length() > 1) {
-                    inicio = linea.substring(0, 2);
-        
-                if (inicio.equals("//")) {
-                    contador++;
+                if (liniaSinEspacios.length() > 0) {
+
+                  if (liniaSinEspacios.length() == 1){
+                    pw.println(linea);
+                  }else{
+                      inicio = liniaSinEspacios.substring(0, 2);
+
+                      if (inicio.equals("//")) {
+                      }else{
+                        pw.println(linea);
+                      }
+                    }
+                  }else{
+                    pw.println("");
+                  }
                 }
+              }
             }
+          }
         }
-        System.out.println(contador);
-        br.close();
+      }
     }
+  }
 }
