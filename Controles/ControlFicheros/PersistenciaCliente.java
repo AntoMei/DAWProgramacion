b9b1f5699a40211c980f49f861486d3b027e5c35
AntoMei <90893283+AntoMei@users.noreply.github.com>
2022-05-01 19:19:12 +0200

    No terminado, hecho el read pero no funciona

--- Controles/ControlFicheros/PersistenciaCliente.java
+++ Controles/ControlFicheros/PersistenciaCliente.java
@@ -2,11 +2,15 @@ package ControlFicheros;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
 
 public class PersistenciaCliente implements Serializable{
 
@@ -35,8 +39,28 @@ public class PersistenciaCliente implements Serializable{
     public void read() throws IOException{
 
        String file = "Controles/ControlFicheros/clientes.dat";
+       String linea;
+       ArrayList<Cliente> lista = new ArrayList<>();
 
-       try(BufferedReader br = new BufferedReader(new FileReader(file)))
+       System.out.println("Leyendo el fichero: " +file);
+
+       try (Scanner teclado = new Scanner(new File(file))){
+
+         while (teclado.hasNextLine()) {
+
+          linea = teclado.nextLine();
+          lista.add(linea);
+          for (int i = 0; i < lista.size(); i++) {
+            System.out.println(lista.get(i));
+          }
+         }
+       } catch (Exception e) {
+         System.out.println(e.getMessage());
+       }
+      }
+    }
+
+          /* try(BufferedReader br = new BufferedReader(new FileReader(file)))
         {
             String line;
             while ((line = br.readLine()) != null) {
