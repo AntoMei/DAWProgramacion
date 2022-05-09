0ef3dd3e893f8035ef90f83317595faf55fdde83
AntoMei <antonio.meirosu444@gmail.com>
2022-05-09 09:35:45 +0200

    Terminado

--- Tema6/Eliminar/EliminarComentario.java
+++ Tema6/Eliminar/EliminarComentario.java
@@ -1,47 +1,62 @@
 package Eliminar;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class EliminarComentario {
-    public static void main(String[] args) throws IOException {
-
-      FileReader fichero = new FileReader("Tema6/Eliminar/Programa.java");
-
-        BufferedReader br = new BufferedReader(fichero);
+  public static void main(String[] args) {
+   /* 
+    if (args.length != 2) {
+      System.out.println("Uso del programa: LimpiarComentarios PROGRAMA_ORIGINAL PROGRAMA_LIMPIO");
+      System.exit(-1);
+    }*/
         
-        PrintWriter escritor = new PrintWriter("Tema 6/Eliminar/ProgramaResultado.java");
+    try {
+      BufferedReader lector = new BufferedReader(new FileReader("Tema6/Eliminar/Programa.java"));
+      BufferedWriter escritor = new BufferedWriter(new FileWriter("Tema6/Elimiar/Prueba.java"));
 
-        String linea;
-        String lineaSinEspacios;
-        String inicio;
+      String lineaOrigen = "";
+      String lineaDestino = "";
+      boolean comentarioDeBloque = false;
+      int i = 0;
+      
+      while ((lineaOrigen = lector.readLine()) != null) {
+        
+        int longitud = lineaOrigen.length();
 
-        while((linea = br.readLine())!= null){
+        if(!comentarioDeBloque)
+            lineaDestino = lineaOrigen;
+        else
+            lineaDestino = "";
 
-            lineaSinEspacios = linea.replaceAll("\\s","");
-
-            if(lineaSinEspacios.length() > 0){
-
-                if(lineaSinEspacios.length() == 1){
-                    escritor.println(linea);
-                }else{
-                    inicio = lineaSinEspacios.substring(0, 2);
-            
-                    if(inicio.equals("//")){
-                    }else{
-                        escritor.println(linea);
-                    }
-                }
-            }else{
-                escritor.println("");
-            }
-            
+        // Detecta inicio de comentario de bloque 
+        if ((i = lineaOrigen.indexOf("/*")) != -1) {
+          comentarioDeBloque = true;
+          lineaDestino = lineaOrigen.substring(0, i);
+          lineaOrigen = lineaOrigen.substring(i+2,longitud);
+        }
+        
+        // Detecta fin de comentario de bloque 
+        if (((i = lineaOrigen.indexOf("*/")) != -1) && comentarioDeBloque){
+          comentarioDeBloque = false;
+          lineaDestino += lineaOrigen.substring(i + 2, longitud);
+        }
+        // Detecta comentario de línea
+        if (((i = lineaOrigen.indexOf("//")) != -1) && !comentarioDeBloque) {
+          lineaDestino = lineaOrigen.substring(0, i);
         }
 
-        escritor.close();
-        fichero.close();
-        br.close();
+        escritor.write(lineaDestino + "\n");
+      }
+      
+      lector.close();
+      escritor.close();
+
+    } catch (IOException ioe) {
+      System.err.println(ioe.getMessage());
     }
+  }
 }
