eddd51c645dd844794db9674387b2f48f2ac25a9
AntoMei <antonio.meirosu444@gmail.com>
2022-05-11 09:59:35 +0200

    Finalizado Escribir Gson

--- Controles/ControlFicheros/EscribirEnFicheroJSON.java
+++ Controles/ControlFicheros/EscribirEnFicheroJSON.java
@@ -11,11 +11,13 @@ public class EscribirEnFicheroJSON {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         
         ObjectOutputStream archivo = new ObjectOutputStream(new FileOutputStream("Controles/ControlFicheros/clientes.json"));
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        for(Cliente cliente: PersistenciaCliente.clientes){
-            String imprimirGson = gson.toJson(cliente);
-            archivo.writeUTF(imprimirGson);
-        }
+        final Cliente c1 = new Cliente("1", "12345678a", "Antonio", "Meirosu", "antonio.meirosu@gmail.com");
+        final Cliente c2 = new Cliente("2", "12345678b", "Ant", "Me", "anto.meirosu@gmail.com");
+        final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        final String imprimir1 = gson.toJson(c1);
+        final String imprimir2 = gson.toJson(c2);
+        archivo.writeUTF(imprimir1);
+        archivo.writeUTF(imprimir2);
         archivo.close();
-    }   
-} 
+    }  
+}
