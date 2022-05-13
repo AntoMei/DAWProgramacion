35f4f3014ca6ee9be91399dd75eb5c8d45089079
AntoMei <antonio.meirosu444@gmail.com>
2022-05-13 09:34:45 +0200

    Extraer los enlaces

--- Tema6/LeerPaginaWeb/LeerPaginaWeb.java
+++ Tema6/LeerPaginaWeb/LeerPaginaWeb.java
@@ -8,11 +8,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LeerPaginaWeb {
 
-    public String LeerPaginaUrl(String paginaUrl){
+    /*public String LeerPaginaUrl(String paginaUrl){
 		URL url;
 		String paginaString = "";
 		try {
@@ -59,7 +63,33 @@ public class LeerPaginaWeb {
 		try {
 			demo.createStaticPage(pageUrl, filePath);
 		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-}
+			e.printStackTrace();*/
+
+    public static void main(String[] args) throws Exception {
+        String content = extractContent("https://www.netflix.com/es/");
+            showLinks(content);
+        }
+          
+        private static String extractContent(String urlString) throws MalformedURLException, IOException {
+            URL url = new URL(urlString);
+            URLConnection urlConnection = url.openConnection();
+            InputStream is = urlConnection.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                String content = "";
+                String linea = br.readLine();
+                while (null != linea) {
+                   content += linea;
+                   linea = br.readLine();
+                }
+                return content;
+             }
+          
+        private static void showLinks(String content) {
+            Pattern pattern = Pattern.compile("(?i)HREF\\s*=\\s*\"(.*?)\"");
+            Matcher matcher = pattern.matcher(content);
+            while (matcher.find()) {
+                System.out.println(matcher.group(1));
+                
+            }
+        }
+    }
