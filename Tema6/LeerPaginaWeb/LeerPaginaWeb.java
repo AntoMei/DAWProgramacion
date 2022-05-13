31a1eab432ff8664208b0a22fc4e5e37eb7fe66b
AntoMei <antonio.meirosu444@gmail.com>
2022-05-13 10:07:58 +0200

    No termiando descargar la imagen

--- Tema6/LeerPaginaWeb/LeerPaginaWeb.java
+++ Tema6/LeerPaginaWeb/LeerPaginaWeb.java
@@ -1,7 +1,9 @@
 package LeerPaginaWeb;
 
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -89,7 +91,24 @@ public class LeerPaginaWeb {
             Matcher matcher = pattern.matcher(content);
             while (matcher.find()) {
                 System.out.println(matcher.group(1));
-                
+            }    
+        }
+
+        private static void descargarImagen() throws IOException{
+            URL url = new URL("https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.png");
+            InputStream in = new BufferedInputStream(url.openStream());
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buf = new byte[1024];
+            int n = 0;
+            while (-1!=(n=in.read(buf)))
+            {
+               out.write(buf, 0, n);
             }
+            out.close();
+            in.close();
+            byte[] response = out.toByteArray();
+            FileOutputStream fos = new FileOutputStream("Tema6/LeerPaginaWeb/Imagen.png");
+            fos.write(response);
+            fos.close();
         }
     }
