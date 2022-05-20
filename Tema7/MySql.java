fcf82d456f0fd215c6c8761c01afecd2bd97fa06
AntoMei <antonio.meirosu444@gmail.com>
2022-05-20 10:37:53 +0200

    No conectado

--- Tema7/MySql.java
+++ Tema7/MySql.java
@@ -2,15 +2,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
 public class MySql{
     public static void main(String[] args) {
         try
         {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/clientes","phpmyadmin2","phpmyadmin2");
+            Connection con=DriverManager.getConnection("jdbc:mysql://192.168.204.168:3306/clientes","phpmyadmin2","phpmyadmin2");
             Statement stmt=con.createStatement();  
-            ResultSet rs=stmt.executeQuery("show databases;");
+            ResultSet rs=stmt.executeQuery("SELECT count(*) from clientes");
             System.out.println("Connected");  
         }
         catch(Exception e)
