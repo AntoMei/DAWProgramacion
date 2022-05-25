3902a3c3b4c937f42673e136d4284008783847a0
AntoMei <antonio.meirosu444@gmail.com>
2022-05-25 08:24:56 +0200

    Finzalizado

--- Controles/Agenda/Test.java
+++ Controles/Agenda/Test.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 public class Test {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         Agenda a1 = new Agenda();
+        a1.pedirUsuarios();
         a1.imprimirListadoTexto();
     }
     
