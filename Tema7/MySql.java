61656cbff2bc97ac291e037f4c05359cf95e7fe4
AntoMei <antonio.meirosu444@gmail.com>
2022-05-27 08:18:25 +0200

    Hecho

--- Tema7/MySql.java
+++ Tema7/MySql.java
@@ -7,14 +7,7 @@ public class MySql{
     public static void main(String[] args) {
         try
         {
-<<<<<<< HEAD
-            Class.forName("com.mysql.jc.jdbc.Driver");
-            Connection con=DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/clientes","phpmyadmin2","phpmyadmin");
-=======
-            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con=DriverManager.getConnection("jdbc:mysql://192.168.204.168:3306/clientes","phpmyadmin2","phpmyadmin2");
->>>>>>> fcf82d456f0fd215c6c8761c01afecd2bd97fa06
             Statement stmt=con.createStatement();  
             ResultSet rs=stmt.executeQuery("SELECT count(*) from clientes");
             System.out.println("Connected");  
