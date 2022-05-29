98e0702d704f03bd05fe0b5b7d5eab020ef85eb3
AntoMei <90893283+AntoMei@users.noreply.github.com>
2022-05-29 18:50:01 +0200

    Terminado

---
+++ Tema4/Estadistica/EstadisticasTextoTest.java
@@ -0,0 +1,12 @@
+package Estadistica;
+
+import java.io.IOException;
+
+public class EstadisticasTextoTest {
+
+  public static void main(String[] args) throws IOException {
+    EstadisticasTexto estadistica = new EstadisticasTexto();
+
+    estadistica.contador();
+  }
+}
